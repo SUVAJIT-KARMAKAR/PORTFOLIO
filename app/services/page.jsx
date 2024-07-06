97fe50c8dc7d52d0ede2e6dacedac7fdd0786b1c
SUVAJIT <suvajitk@192.168.1.4>
2024-07-07 00:58:33 +0530

    SUVAJIT KARMAKAR | Services page ready

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,9 +1,86 @@
 // PAGES @IMPORT 
+"use client";
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+// SERVICES OBJECTS 
+const services = [
+    {
+        num: '01',
+        title: 'WEB DEVELOPMENT',
+        description: '',
+        href: "/"
+    },
+
+    {
+        num: '02',
+        title: 'UI / UX DESIGN',
+        description: '',
+        href: "/"
+    },
+
+    {
+        num: '03',
+        title: 'LOGO DESIGN',
+        description: '',
+        href: "/"
+    },
+
+    {
+        num: '04',
+        title: 'SEO',
+        description: '',
+        href: "/"
+    },
+
+];
 
 const Services = () => {
     return (
-        <section className="flex justify-center">
-            <h2> STAY TUNED FOR THIS PAGE </h2>
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+            <div className="container mx-auto">
+                <motion.div 
+                    initial={{ opacity: 0 }}
+                    animate ={{
+                        opacity: 1,
+                        transition: {
+                            delay: 2.4,
+                            duration: 0.4,
+                            ease: "easeIn"
+                        }
+                    }}
+                    className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+                >
+                    {services.map(( service, index ) => {
+                        return (
+                            <div 
+                                key={index} 
+                                className="flex-1 flex flex-col justify-center gap-6 group"
+                            >
+
+                                {/* TOP-SECTION-SERVICES */}
+                                <div className="w-full flex justify-between items-center">
+                                    <div className="text-5xl font-extrabold text-accent"> {service.num} </div>
+                                    <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                                        <BsArrowDownRight className="text-primary text-3xl"/>
+                                    </Link>
+                                </div>
+
+                                {/* HEADING-SECTION-SERVICES */}
+                                <h2 className="text-3xl font-bold text-white group-hover:text-accent transition-all duration-500"> { service.title } </h2>
+
+                                {/* DESCRIPTION-SECTION-SERVICES */}
+                                <p> { service.description } </p>
+
+                                {/* BORDER */}
+                                <div className="border-b border-white/50 w-full"></div>
+
+                            </div>
+                        )
+                    })}
+                </motion.div>
+            </div>
         </section>
     );
 };
