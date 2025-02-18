39ce54d30305e0558b289963799093999d6acd98
suvajitkarmakar <suvajit2001k@gmail.com>
2025-02-18 15:29:42 +0530

    animation(gsap): onscroll animation trigger implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,21 +48,25 @@ const Footer = () => {
       <div className="container">
         <div className="lg:grid lg:grid-cols-2">
           <div className="mb-10">
-            <h2 className="headline-one mb-8 lg:max-w-[12ch]">
+            <h2 className="headline-one mb-8 lg:max-w-[12ch] revealup">
               {" "}
               Let&apos;s work together today!
             </h2>
-            <ButtonPrimary href="mailto:" label="Start a project" />
+            <ButtonPrimary
+              href="mailto:"
+              label="Start a project"
+              classes="revealup"
+            />
           </div>
           <div className="grid grid-cols-2 gap-4 lg:pl-20">
             <div>
-              <p className="mb-2"> SITEMAP </p>
+              <p className="mb-2 revealup"> SITEMAP </p>
               <ul>
                 {sitemap.map(({ label, href }, key) => (
                   <li key={key}>
                     <a
                       href={href}
-                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 revealup"
                     >
                       {label}
                     </a>
@@ -72,14 +76,14 @@ const Footer = () => {
             </div>
 
             <div>
-              <p className="mb-2"> SOCIALS </p>
+              <p className="mb-2 revealup"> SOCIALS </p>
               <ul>
                 {socials.map(({ label, href }, key) => (
                   <li key={key}>
                     <a
                       href={href}
                       target="_blank"
-                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 revealup"
                     >
                       {label}
                     </a>
@@ -90,10 +94,10 @@ const Footer = () => {
           </div>
         </div>
         <div className="flex items-center justify-between pt-20 mb-8">
-          <a href="" className="">
+          <a href="/" className="logo revealup">
             <img src="/avatar/LOGO.png" alt="Logo" width={40} height={40} />
           </a>
-          <p className="text-zinc-500 text-sm">
+          <p className="text-zinc-500 text-sm revealup">
             &copy; 2025 <span className="text-zinc-200">suvajitkarmakar</span>
           </p>
         </div>
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -4,6 +4,14 @@ import PropTypes from "prop-types";
 // COMPONENTS
 import ReviewCard from "./ReviewCard";
 
+// GSAP
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+// GSAP PLUGIN REGISTRATION
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 // REVIEW ITEMS
 const reviews = [
   {
@@ -13,14 +21,51 @@ const reviews = [
     imgSrc: "/testimonials/testimonial_male.png",
     company: "NIT Silchar",
   },
+  {
+    content: "Great developer, totally satisfied with the engineering skills",
+    name: "anirudda chowdhury",
+    imgSrc: "/testimonials/testimonial_male.png",
+    company: "MIT",
+  },
+  {
+    content: "Great at the work, on time delivery with great quality",
+    name: "hemangshu dey",
+    imgSrc: "/testimonials/testimonial_male.png",
+    company: "codewalnut",
+  },
+  {
+    content: "Totally worth choosing this guy",
+    name: "chandrima karmakar",
+    imgSrc: "/testimonials/testimonial_female.png",
+    company: "dav",
+  },
+  {
+    content: "Quality is great, totally cooperative, great with dealines",
+    name: "soumik hembram",
+    imgSrc: "/testimonials/testimonial_male.png",
+    company: "linkedin",
+  },
 ];
 
 const Review = ({ content, imgSrc, name, company }) => {
+  // GSAP
+  useGSAP(() => {
+    gsap.to(".scrubslide", {
+      scrollTrigger: {
+        trigger: ".scrubslide",
+        scrub: true,
+        start: "-200% 80%",
+        end: "400% 80%",
+      },
+      x: "-1000",
+    });
+  });
+
   return (
     <section id="reviews" className="section overflow-hidden">
       <div className="container">
-        <h2 className="headline-two mb-8"> What my customers say </h2>
-        <div className="flex items-stretch gap-3 w-fit">
+        <h2 className="headline-two mb-8 revealup"> What my customers say </h2>
+        <div className="scrubslide flex items-stretch gap-3 w-fit">
           {reviews.map(({ content, name, imgSrc, company }, key) => (
             <ReviewCard
               key={key}
